9e4e0f977bb85989c130fd1de48c1d79505f5992
Carlos Vasconcelos <carlosevpereira8@gmail.com>
2026-05-19 23:14:04 -0300

    feat: adding array and correcting from 16 to 18 years old limit

--- assets/js/avaliacao.js
+++ assets/js/avaliacao.js
@@ -1,6 +1,10 @@
 const avaliacaoEstrela = document.getElementById('avaliacaoEstrela');
 const mensagemAvalicacao = document.getElementById('avaliacaoMensagem');
 let selecionado = 0;
+const mensagens = ['Perfeito! Muito obrigado pela nota máxima!',
+                    'Ótimo! O que faria ser 5 estrelas?', 
+                    'O que podemos fazer melhor?', 
+                    'Lamentamos muito. O que podemos fazer para melhorar?' ]
 
 for (let i = 1; i <= 5; i++) {
   const btn = document.createElement('button');
@@ -15,13 +19,13 @@ for (let i = 1; i <= 5; i++) {
     selecionado = n;
     colorirBotao(n);
     if (n === 5) {
-      mensagemAvalicacao.textContent = 'Perfeito! Muito obrigado pela nota máxima!';
+      mensagemAvalicacao.textContent = mensagens[0];
     } else if (n >= 3) {
-      mensagemAvalicacao.textContent = 'Ótimo! O que faria ser 5 estrelas?';
+      mensagemAvalicacao.textContent = mensagens[1];
     } else if (n >= 2) {
-      mensagemAvalicacao.textContent = 'O que podemos fazer melhor?';
+      mensagemAvalicacao.textContent = mensagens[2];
     } else {
-      mensagemAvalicacao.textContent = 'Lamentamos muito. O que podemos fazer?';
+      mensagemAvalicacao.textContent = mensagens[3];
     }
   });
 
